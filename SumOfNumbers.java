0d53d184aa13626df26e859a430e43822d800791
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-07-27 01:56:58 -0700

    Create SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		Scanner s = new Scanner(System.in);
+		int sum =0,i=1;
+		int n=s.nextInt();
+		while(i<=n)
+		{
+			sum=sum+i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
