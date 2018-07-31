835dd67102c113afdab49fdaa63603efe7307713
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-07-30 23:39:50 -0700

    Create arrayinteger.java

---
+++ arrayinteger.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+             Scanner s = new Scanner(System.in);
+	        int N=s.nextInt();
+	        int K=s.nextInt();
+		int sum=0;
+		int n1;
+		for(int i=1;i<=N;i++)
+		{
+			n1=s.nextInt();
+		}
+		for(int i=1;i<=K;i++)
+		{
+		              sum=sum+i;
+		}
+			System.out.println(sum);
+	}
+
+}
