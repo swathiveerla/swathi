42616b554add6b2e4e8f596e168c2e821d002aab
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-07 05:03:00 -0700

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int d=sc.nextInt();
+		int val=0;
+		val=(n*(2*a+(n-1)*d))/2;
+		System.out.println(val);
+	}
+}
