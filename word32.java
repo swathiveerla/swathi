58fc8039b6808ffec7cf77735a73f873430b19a2
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-08 00:53:57 -0700

    Create word32.java

---
+++ word32.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String st;
+		Scanner s=new Scanner(System.in);
+		st=s.nextLine();
+		int world=0;
+		     String[] st1=st.split(" ");
+		     System.out.print(st1.length);
+	}
+}
