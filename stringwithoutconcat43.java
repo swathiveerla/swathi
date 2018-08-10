784c3ddbb2eaa7e1fc1fde45fc0cc81a44be40ef
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-10 00:16:11 -0700

    Create stringwithoutconcat43.java

---
+++ stringwithoutconcat43.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String st1,st2,sum;
+		Scanner s=new Scanner(System.in);
+		 st1=s.next();
+		 st2=s.next();
+		{
+			sum=st1+st2;
+		}
+		System.out.println(sum);
+	}
+}
