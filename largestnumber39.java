7e9f0be6bb1592416ce6d2b84d62a5aeb22644d6
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-14 03:59:09 -0700

    Create largestnumber39.java

---
+++ largestnumber39.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int max;
+		int a[]=new int[10];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		max=a[0];
+		for(int i=0;i<a.length;i++)
+		{
+			if(max<a[i])
+			{
+				max=a[i];
+			}
+		}
+		System.out.print(max);
+	}
+}
