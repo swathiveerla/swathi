c940e8cbbef4ba3aba56e9fa5657d758e9162550
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-15 10:54:10 -0700

    Create length42.java

---
+++ length42.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String st1,st2;
+		Scanner s=new Scanner(System.in);
+		st1=s.next();
+		st2=s.next();
+		if(st1.length()>=st2.length())
+		{
+			System.out.println(st1);
+		}
+		else
+		{
+			System.out.println(st2);
+		}
+	}
+}
