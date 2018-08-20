2cbcfaac5489618202b08139031a0b5be407c8f6
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-20 03:39:50 -0700

    Create binary62.java

---
+++ binary62.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+             int Count=0;
+             Scanner s = new Scanner(System.in);
+	 int num = s.nextInt();
+	 String number = String.valueOf(num);
+	 char [] numArray= number.toCharArray();
+	 for(char checkBinary :numArray )
+	{
+	   if(!(checkBinary=='1' || checkBinary=='0'))
+               {
+		Count++;
+               }
+         
+            }
+              if(Count>0)
+            {
+              System.out.println("No");
+             }
+            else
+             System.out.println("yes");
+	}
+}
