da4461d97e1b3ce0d523094c6eabad17d543a7ad
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-22 03:28:40 -0700

    Create temp93.java

---
+++ temp93.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int c=s.nextInt();
+	             double num=0;
+			num=(c+273.15);
+			System.out.println((int)num);
+	}
+}
