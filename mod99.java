67e2ae9905d6021c590393a060dd83a65f72cb33
swathiveerla <41728984+swathiveerla@users.noreply.github.com>
2018-08-22 21:41:20 -0700

    Create mod99.java

---
+++ mod99.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		int sum=0;
+		int res=a*b%c;
+		System.out.println(res);
+		// your code goes here
+	}
+}
